1873202527cc05ed6fe326b5dbca47f7c59b9a79
Philip Pemberton <philpem@philpem.me.uk>
2010-01-24 18:59:20 +0000

    massive refactor

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <cstring>
 #include <vector>
+#include <list>
 #include <iostream>
 #include <exception>
 
@@ -10,31 +11,95 @@ using namespace std;
 
 class Chunk {
 	private:
-		vector<Chunk> children;
-		char chunktype[4];
+		list<Chunk *> children;
 	public:
-		vector<uint8_t> payload;
+		typedef list<Chunk *>::size_type childID_T;
 
-		Chunk();
+		virtual ~Chunk(){ cerr<<"Destructor: Chunk"<<endl;}
+
+		virtual std::string getChunkType() const =0;
+
+		void addChild(Chunk *c);
+		void eraseChild(childID_T id);
+		childID_T getChildCount();
+		void clearChildren();
 
 		virtual vector<uint8_t> serialise(void);
-		void addChild(const Chunk c);
-		void setChunktype(const char *ct);
 };
 
-class EBadChunktype : public exception {
-	virtual const char* what() { return "bad chunk type specified"; }
+class XDIFChunk : public Chunk {
+	public:
+		virtual std::string getChunkType() const { return "XDIF"; };
+		virtual ~XDIFChunk(){ cerr<<"Destructor: XDIFChunk"<<endl;}
+
+		// serialise is left as-is because this node contains other nodes,
+		// not payload data
 };
 
-// ctor for Chunk
-Chunk::Chunk()
-{
-	// clear chunk type
-	for (size_t i=0; i<sizeof(chunktype); i++) {
-		chunktype[i] = ' ';
+class METAChunk : public Chunk {
+	public:
+		vector<uint8_t> payload;
+		
+		virtual ~METAChunk(){ cerr<<"Destructor: METAChunk"<<endl;}
+
+		virtual std::string getChunkType() const { return "META"; };
+		virtual vector<uint8_t> serialise(void) {
+			vector<uint8_t> data;
+
+			// store the chunk type
+			string chunktype = this->getChunkType();
+			for (string::size_type i=0; i<chunktype.size(); i++) {
+				data.push_back(chunktype[i]);
+			}
+
+			// store payload size
+			data.push_back(payload.size() >> 56	& 0xff);
+			data.push_back(payload.size() >> 48	& 0xff);
+			data.push_back(payload.size() >> 40	& 0xff);
+			data.push_back(payload.size() >> 32	& 0xff);
+			data.push_back(payload.size() >> 24	& 0xff);
+			data.push_back(payload.size() >> 16	& 0xff);
+			data.push_back(payload.size() >> 8	& 0xff);
+			data.push_back(payload.size()		& 0xff);
+
+			// store payload
+			data.insert(data.end(), payload.begin(), payload.end());
+
+			return data;
+		}
+};
+
+// exception for the factory
+class EBadChunkType : public exception {
+	virtual const char *what() { return "chunk ID invalid, cannot build!"; }
+};
+
+// A really kooky implementation of a variant of the factory design pattern
+// (or possible abstract factory?)
+class ChunkFactory {
+	// build a new chunk of the specified type
+	static Chunk *build(string id) {
+		if		(id.compare("XDIF")==0) return new XDIFChunk();
+		else if	(id.compare("META")==0) return new METAChunk();
+		else throw new EBadChunkType();
 	}
-}
 
+	// clone an existing chunk
+	static Chunk *build(Chunk &c) {
+		if		(c.getChunkType().compare("XDIF")==0)	return new XDIFChunk(dynamic_cast<XDIFChunk&>(c));
+		else if	(c.getChunkType().compare("META")==0)	return new METAChunk(dynamic_cast<METAChunk&>(c));
+		else throw new EBadChunkType();
+	}
+};
+
+/****************************************************************************/
+
+/*
+// dtor for Chunk
+Chunk::~Chunk()
+{
+}
+*/
 // serialise a chunk
 vector<uint8_t> Chunk::serialise(void)
 {
@@ -42,7 +107,8 @@ vector<uint8_t> Chunk::serialise(void)
 	vector<uint8_t> data;
 
 	// store the chunk type
-	for (size_t i=0; i<sizeof(this->chunktype); i++) {
+	string chunktype = this->getChunkType();
+	for (string::size_type i=0; i<chunktype.size(); i++) {
 		data.push_back(chunktype[i]);
 	}
 
@@ -60,15 +126,11 @@ vector<uint8_t> Chunk::serialise(void)
 		data.at(3) |= 0x80;
 
 		// serialise children (depth-first traversal)
-		for (vector<Chunk>::size_type i=0; i<this->children.size(); i++) {
-			vector<uint8_t> x = this->children.at(i).serialise();
+		for (list<Chunk *>::iterator i=this->children.begin(); i != this->children.end(); i++) {
+			vector<uint8_t> x = (*i)->serialise();
 			data.insert(data.end(), x.begin(), x.end());
 			chunksize += x.size();
 		}
-	} else {
-		// no, store the data instead
-		data.insert(data.end(), this->payload.begin(), this->payload.end());
-		chunksize += this->payload.size();
 	}
 
 	// store the chunk length
@@ -85,48 +147,56 @@ vector<uint8_t> Chunk::serialise(void)
 	return data;
 }
 
-void Chunk::addChild(const Chunk c)
+// add a new child
+void Chunk::addChild(Chunk *c)
 {
 	this->children.push_back(c);
 }
 
-void Chunk::setChunktype(const char *ct)
+// delete child
+void Chunk::eraseChild(Chunk::childID_T id)
 {
-	// check chunktype length
-	if (strlen(ct) != 4) throw new EBadChunktype();
+	list<Chunk *>::iterator i = this->children.begin();
+	for (Chunk::childID_T s=0; s<id; s++) i++;
+	this->children.erase(i);
+}
 
-	// make sure chunktype is valid (printables only)
-	for (size_t i=0; i<4; i++) {
-		if (ct[i] >= 0x80) throw new EBadChunktype();
-	}
+// get number of children
+Chunk::childID_T Chunk::getChildCount()
+{
+	return this->children.size();
+}
 
-	// now copy the chunktype into the buffer
-	for (size_t i=0; i<4; i++) {
-		this->chunktype[i] = ct[i];
-	}
+// clear all children
+void Chunk::clearChildren()
+{
+	this->children.clear();
 }
 
 int main(void)
 {
-	Chunk ch;
-	ch.setChunktype("xDIF");
+	Chunk *ch = new XDIFChunk();
 
-	Chunk *meta = new Chunk();
-	meta->setChunktype("META");
+	METAChunk *meta = new METAChunk();
 	meta->payload.push_back('f');
 	meta->payload.push_back('o');
 	meta->payload.push_back('o');
 	meta->payload.push_back('b');
 	meta->payload.push_back('a');
 	meta->payload.push_back('r');
-	ch.addChild(*meta);
-	delete meta;
+	ch->addChild(meta);
+//	delete meta;
 
 	// dump data to stdout
-	vector<uint8_t> data = ch.serialise();
+	vector<uint8_t> data = ch->serialise();
 	for (vector<uint8_t>::size_type i=0; i<data.size(); i++) {
 		cout << data[i];
 	}
+
+	cerr << "Deleting ch..." << endl;
+	delete ch;
+	cerr << "Deleting meta..." << endl;
+	delete meta;
 
 	return 0;
 }
